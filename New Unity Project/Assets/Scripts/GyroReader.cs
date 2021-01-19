ba4fed3d95c1a8a8731b6edaa5b90e4c9ad2a816
HappyFaceFriend <yjl980730@gmail.com>
2021-01-19 23:26:24 +0900

    rotating hand by values read through arduino

---
+++ New Unity Project/Assets/Scripts/GyroReader.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroReader : MonoBehaviour
+{
+    public SerialController serialController;
+    public HandControler hand;
+
+    bool firstTimeReading = true;
+    public Vector3 offset;
+    public Vector3 error;
+    Vector3 lastInput;
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        ReadMessage();
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            firstTimeReading = true;
+        }
+    }
+    void ReadMessage()
+    {
+        string message = serialController.ReadSerialMessage();
+        if (message == null)
+            return;
+
+        if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+            Debug.Log("Connection established");
+        else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+            Debug.Log("Connection attempt failed or disconnection detected");
+        else
+        {
+            if (message[0] == '#')
+                DecryptMessage(message);
+            else
+                Debug.Log("System message : " + message);
+        }
+    }
+    void DecryptMessage(string message)
+    {
+
+        string[] s = message.Substring(1).Split('/');
+        //Vector3 inputVector = new Vector3(-float.Parse(s[2]), float.Parse(s[0]), float.Parse(s[1]));
+        Vector3 inputVector = new Vector3(-float.Parse(s[2]), float.Parse(s[0]), float.Parse(s[1]));
+
+        if (firstTimeReading)
+        {
+            offset = inputVector;
+            lastInput = inputVector;
+            firstTimeReading = false;
+            error = Vector3.zero;
+        }
+        else
+        {
+            Debug.Log(Mathf.Abs(inputVector.y - lastInput.y));
+            if (Mathf.Abs(inputVector.y - lastInput.y) <= 0.1f)
+                error.y += inputVector.y - lastInput.y;
+            hand.FeedVector(inputVector - offset - error);
+            lastInput = inputVector;
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/HandControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    public void FeedVector(Vector3 inputVector)
+    {
+        transform.rotation = Quaternion.Euler(inputVector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
